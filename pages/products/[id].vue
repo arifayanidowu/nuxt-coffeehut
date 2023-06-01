d3c1ee3c74fd305bbd3cb43df112ac97f4b4fce9
stizzle123 <arifayanidowu2@yahoo.com>
2023-06-01 14:37:36 +0100

    updated product/[id]

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,8 +1,31 @@
 <template>
-  <div>
+  <div class="text-center">
+    <!-- Product image -->
+    <NuxtImg
+      :src="item?.image"
+      :alt="item?.name"
+      class="w-full h-60 object-cover object-center"
+      loading="lazy"
+    />
     <!-- Product details -->
-    <h1>{{ item?.name }}</h1>
-    <p>{{ item?.description }}</p>
+    <div class="leading-8 mt-4">
+      <h1>{{ item?.name }}</h1>
+      <p>{{ item?.description }}</p>
+      <p>${{ item?.price?.toFixed(2) }}</p>
+    </div>
+    <!-- Add to cart button -->
+    <button
+      v-if="item"
+      class="snipcart-add-item bg-orange-500 text-white rounded-md px-4 py-2 mt-4 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-orange-100 focus:ring-offset-orange-800"
+      :data-item-id="item.id"
+      :data-item-price="item.price"
+      :data-item-description="item.description"
+      :data-item-image="item.image"
+      :data-item-name="item.name"
+      :data-item-url="`/products/${item.id}`"
+    >
+      Add to cart
+    </button>
   </div>
 </template>
 
